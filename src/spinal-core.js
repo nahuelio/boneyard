0897a61c5895b9788f15fcb439a4ece8e0f1352b
kuakman <3dimentionar@gmail.com>
2014-02-23 01:09:45 +0000

    Added Test cases in the core module of Spinal (Inheritance, Extend )deep copy) and Generic Interface for constructor functions.

--- src/spinal-core.js
+++ src/spinal-core.js
@@ -2,13 +2,17 @@
 *  Spinal UI Framework
 *  @author Patricio Ferrerira <3dimentionar@gmail.com>
 **/
-var Spinal = (function() {
-    
+"use strict";
+
+(function(exports) {
+	
+	var	dateiso = /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d)|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d)/;
+	
 	/**
 	*	Namespacing Strategy
 	**/
-	var namespace = function(path, constructor) {
-		var parts = path.split('.'), parent = Spinal, pl, i;
+	exports.namespace = function(path, constructor) {
+		var parts = path.split('.'), parent = exports, pl, i;
 		if (parts[0] == "spinal") parts = parts.slice(1);
 		pl = parts.length;
 		for (var i = 0; i < pl; i++) {
@@ -20,36 +24,100 @@ var Spinal = (function() {
 	};
 	
 	/**
-	*	Inheritance Strategy
+	*	JSON Serialization Strategy
 	**/
-    var inherit = function(proto, static) {
-        var Parent = this, Child;
-    
+	var _serialize = function(k, v) {
+		if(this[k] instanceof Date) return this[k].toJSON();
+		if(this[k] === undefined) return null;
+		if(this[k] instanceof Function) return v.toString();
+		return v;
+	};
+	
+	/**
+	*	JSON Deserialization Strategy
+	**/
+	var _deserialize = function(k, v) {
+		if(dateiso.test(v)) return new Date(v);
+		if(v && v.substring && v.substring(0, 8) === 'function') return eval('(' + v + ')');
+		return v;
+	};
+	
+	/**
+	*	Parse first level of properties
+	**/
+	var _parse = function(from) {
+		var sd = JSON.parse(JSON.stringify(from, _serialize), _deserialize);
+		for(var p in from) {
+			if(!this.hasOwnProperty(p)) this[p] = sd[p];
+		}
+	};
+		
+	/**
+	*	Spinal Deep Copy strategy for inheritance.
+	*	Unsupported types: RegExp.
+	*	@BECHMARK See how performance diagnostic we get with this.
+	**/
+	exports._extend = function(o) {
+		var args = Array.prototype.slice.call(arguments, 1);
+		for(var i = 0; i < args.length; i++) {
+			if(args[i]) _parse.call(o, args[i]);
+		}
+		return o;
+	};
+	
+	/** Generic Class Constructor & Interfaces **/
+	
+	/**
+	*	Basic Constructor Function
+	**/
+	var _constructor = function(attrs) {
+		attrs || (attrs = {});
+		this.attributes = {};
+		this.set(attrs);
+		this.initialize.apply(this, arguments);
+	};
+	
+	/**
+	*	Basic Spinal Interface for Classes.
+	**/
+	var _interface = {
+		initialize: function() { return this; },
+		get: function(p) { return this.atributes[p]; },
+		set: function(p, v) {
+			if(!p) throw new Error('set() requires 1 arguments (object or a key).');
+			if(p && p === Object(p)) {
+				exports._extend.call(this.attributes, p);
+			} else {
+				this.attributes[p] = v;
+			}
+			return this;
+		}
+	};
+	
+	/**
+	*	Inheritance Strategy
+	*	FIXME (Not working right).
+	**/
+    exports.inherit = function(proto, protoStatic) {
+        var Parent = this, Child = function() { return Parent.apply(this, arguments); };
+		
+		if(protoStatic) exports._extend(Child, Parent, protoStatic);
         var F = function() { this.constructor = Child; };
         F.prototype = Parent.prototype;
-        Child = function() { Parent.apply(this, arguments); };
         Child.prototype = new F();
-  
-        if(proto) for(var i in proto) Child.prototype[i] = proto[i];
-      
-        Child.__super__ = Parent.constructor;
+		Child.inherit = exports.inherit;
+      	if(proto) exports._extend(Child.prototype, proto);
+		
+        Child.__super__ = Parent.prototype;
         return Child;
     };
 	
-	/**
-	*	Interface uses Strategy
-	**/
-	var uses = function() {
-		// IMPLEMENT
-	};
-
-    return {
-        __VERSION__: '<%= version %>',
-		namespace: namespace,
-        inherit: inherit,
-		uses: uses
-    };
-
-}());
-
-if(module) module.exports = Spinal;
+	// Default Generic Class
+	// See if we can reuse Backbone.Events
+	exports.Generic = _constructor;
+	exports._extend(exports.Generic.prototype, _interface);
+	exports.Generic.inherit = exports.inherit;
+			
+	exports.__VERSION__ = '<%= version %>';
+	
+}(typeof exports === 'undefined' ? (window.Spinal = {}) : exports));
--- test/spinal-core-test.js
+++ test/spinal-core-test.js
@@ -4,7 +4,8 @@
 **/
 var libpath = process.env['UT'] ? 'lib-cov' : 'lib',
     path = require('path'),
-    should = require('should');
+    should = require('should'),
+	_ = require('underscore');
 
 var Spinal = require(path.resolve(libpath, 'spinal-core'));
 
@@ -13,6 +14,18 @@ var Spinal = require(path.resolve(libpath, 'spinal-core'));
 *    @classdesc Unit test for core Spinal operations
 **/
 describe('Spinal', function() {
+	
+	before(function() {
+		this.ds = { _string: 'string' };
+		this.dn = { _number: 1 };
+		this.db = { _boolean: true };
+		this.du = { _undefined: undefined };
+		this.dl = { _null: null };
+		this.do = { _object: { _string: 'string', _function: function() { console.log('parent.func()', this._string); }} };
+		this.da = { _array: ['a', 1, true, undefined, null, { _string: 'a-string' }, function() { var t = 'local'; console.log('parent.func()', local); }] };
+		this.dd = { _date: new Date() };
+		this.df = { _string: 'test', _function: function() { return { _string: this._string }; } };
+	});
 	
 	/**
 	*	Creates Namespaces
@@ -28,20 +41,112 @@ describe('Spinal', function() {
 	});
 	
 	/**
-	*	Spinal Inheritance strategy
+	*	Spinal Deep Copy strategy
 	**/
-	describe('#inherit()', function() {
-		it('Should Inherit Class', function() {
-			
+	describe('#_extend()', function() {
+		
+		it('Should return a deep copy of an object (1 on 1)', function() {
+			var c = Spinal._extend({ _string: 'child_string' }, this.ds);
+			should.exist(c);
+		});
+		
+		it('Should return a deep copy of an object (multiple to 1)', function() {
+			var c = Spinal._extend({ _string: 'child_string', _number: 2 }, this.ds, this.dn);
+			should.exist(c);
+			c._string.should.equal('child_string');
+			c._number.should.equal(2);
+			should.not.exist(this.ds._number);
+			should.not.exist(this.dn._string);
+			this.ds._string.should.equal('string');
+			this.dn._number.should.equal(1);
+		});
+		
+		// String Type
+		it('Should return a deep copy of an object having a String', function() {
+			var c = Spinal._extend({ _string: 'child_string' }, this.ds);
+			c._string.should.not.equal(this.ds._string);
+		});
+		
+		// Number Type
+		it('Should return a deep copy of an object having a Number', function() {
+			var c = Spinal._extend({ _number: 1.10 }, this.dn);
+			c._number.should.not.equal(this.dn._number);
+		});
+		
+		// Boolean Type
+		it('Should return a deep copy of an object having a Boolean', function() {
+			var c = Spinal._extend({ _boolean: false }, this.db);
+			c._boolean.should.be.false;
+			this.db._boolean.should.be.true;
+		});
+		
+		// Undefined Type
+		it('Should return a deep copy of an object having a Undefined', function() {
+			var c = Spinal._extend({ _undefined: 'notUndefined' }, this.du);
+			c._undefined.should.not.equal(this.du._undefined);
+			c._undefined.should.equal('notUndefined');
+		});
+		
+		// Null Type
+		it('Should return a deep copy of an object having a Null', function() {
+			var c = Spinal._extend({ _null: 'notNull' }, this.dnl);
+			c._null.should.not.equal(this.du._null);
+			c._null.should.equal('notNull');
+		});
+		
+		// Object Type
+		it('Should return a deep copy of an object having a Object', function() {
+			var c = Spinal._extend({ _object: { _string: 'myString', _function: function() { console.log('child.func()', this._string); } }}, this.do);
+			should.exist(c._object);
+			should.exist(c._object._string);
+			c._object._string.should.equal('myString');
+			this.do._object._string.should.not.equal(c._object._string);
+		});
+		
+		// Array Type
+		it('Should return a deep copy of an object having a Array', function() {
+			var c = Spinal._extend({ _array: [2, { _string: 'myString' }] }, this.da);
+			should.exist(c._array);
+			should.exist(c._array[1]);
+			c._array.length.should.not.equal(this.da._array.length);
+			c._array[0].should.not.equal(this.da._array[0]);
+		});
+		
+		// Date Type
+		it('Should return a deep copy of an object having a Date', function() {
+			var c = Spinal._extend({ _date: new Date('2014-02-22T23:43:51.223Z') }, this.dd);
+			should.exist(c._date);
+			c._date.toISOString().should.equal('2014-02-22T23:43:51.223Z');
+			c._date.toISOString().should.not.equal(this.dd._date.toISOString());
+		});
+		
+		// Function Type
+		it('Should return a deep copy of an object having a Function', function() {
+			var c = Spinal._extend({ _prop: 1, _function: function() { var ls = 'local'; return { ls: ls, prop: this._prop, _string: this._string }; } }, this.df);
+			should.exist(c._function);
+			var childData = c._function(), parentData = this.df._function();
+			childData.ls.should.equal('local');
+			childData.prop.should.equal(1);
+			childData._string.should.equal(parentData._string);
+			// Scope verification
+			c._string = 'changed'; // Change of variable _string in the subclass
+			childData = c._function(); // call the function to extract this._string (scope of the function).
+			childData._string.should.not.equal(parentData._string); // verification if c._string is not a reference of this.df._string after deep copy.
 		});
 	});
 	
 	/**
-	*	Spinal interface usage
+	*	Spinal Inheritance strategy
 	**/
-	describe('#uses()', function() {
-		it('Should use an interface class', function() {
-			
+	describe('#inherit()', function() {
+		it('Should Inherit Class', function() {
+			var SubClass = Spinal.Generic.inherit({
+				_string: 'sub',
+				_object: { _boolean: false, _date: new Date('2014-02-22T23:43:51.223Z') },
+				submethod: function() { return this._string + 'class'; }
+			}, { NAME: 'SubClass' });
+			var instance = new SubClass;
+			console.log(instance);
 		});
 	});
 	
