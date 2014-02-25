09317e16693eb8ed09912c9908dee9fd921fc1a4
kuakman <3dimentionar@gmail.com>
2014-02-25 22:12:13 +0000

    Updated benchmark templates and skin. Update underscore library to use 1.5.x compatible with backbone 1.1.0.

--- benchmark/libs/core/core-bm.js
+++ benchmark/libs/core/core-bm.js
@@ -4,14 +4,28 @@
 **/
 $(document).ready(function() {
 	
-	/** Spinal.namespace('a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.y.z.End', function() { }); **/
-	
 	Bench.suite.add('core#namespace', function() {
-		Spinal.namespace('a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.y.z.End', function() { });
+		Spinal.namespace('a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.y.z.End', function() {});
 	}, Bench._events);
 	
 	Bench.suite.add('core#_extend', function() {
-		
+		Spinal._extend({}, {
+			str: 'one',
+			num: 1
+		}, {
+			func: function() { },
+			bol: true
+		}, {
+			obj: { str: 'two', num: 2 },
+			nul: null
+		}, {
+			dat: new Date(),
+			und: undefined
+		}, {
+			arr: ['1','2','3','4','5']
+		}, {
+			arr: [{ str: 'three' }, { num: 3 }, { func: function() { } }, { bol: false }, { dat: new Date() }]
+		});
 	}, Bench._events);
 	
 	Bench.suite.add('core#inherit', function() {
@@ -19,5 +33,5 @@ $(document).ready(function() {
 	}, Bench._events);
 	
 	Bench.suite.emit('created');
-	
+    
 });
