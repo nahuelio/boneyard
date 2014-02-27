4f0212e48ae4d312558875277bf81f90ab6926ae
kuakman <3dimentionar@gmail.com>
2014-02-27 00:17:57 +0000

    Added interface methods to View and Container Classes.

--- src/com/spinal/ui/container.js
+++ src/com/spinal/ui/container.js
@@ -16,12 +16,83 @@ var Container = Spinal.namespace('com.spinal.ui.Container', View.inherit({
 	*	@public
 	*	@chainable
 	*	@method initialize
-	*	@return {com.spinal.ui.SpinalContainer}
+	*	@return {com.spinal.ui.Container}
 	**/
 	initialize: function() {
 		Container.__super__.initialize.apply(this, arguments);
 		return this;
-	}
+	},
+	
+	/**
+	*	Add View
+	*	@public
+	*	@chainable
+	*	@method add
+	*	@return {com.spinal.ui.Container}
+	**/
+	add: function() { },
+	
+	/**
+	*	Remove View
+	*	@public
+	*	@chainable
+	*	@method lookup
+	*	@return {com.spinal.ui.Container}
+	**/
+	remove: function() { },
+	
+	/**
+	*	Find View
+	*	@public
+	*	@method find
+	*	@return Any
+	**/
+	find: function() { },
+	
+	/**
+	*	Show View
+	*	@public
+	*	@chainable
+	*	@method show
+	*	@return {com.spinal.ui.View}
+	**/
+	show: function() { },
+	
+	/**
+	*	Hide View
+	*	@public
+	*	@chainable
+	*	@method hide
+	*	@return {com.spinal.ui.View}
+	**/
+	hide: function() { },
+	
+	/**
+	*	Enable View
+	*	@public
+	*	@chainable
+	*	@method enable
+	*	@return {com.spinal.ui.View}
+	**/
+	enable: function() { },
+	
+	/**
+	*	Disable View
+	*	@public
+	*	@chainable
+	*	@method disable
+	*	@return {com.spinal.ui.View}
+	**/
+	disable: function() { },
+	
+	/**
+	*	Clear View
+	*	@public
+	*	@chainable
+	*	@method clear
+	*	@return {com.spinal.ui.View}
+	**/
+	clear: function() { }
 	
 }, {
 	
--- src/com/spinal/ui/view.js
+++ src/com/spinal/ui/view.js
@@ -20,7 +20,79 @@ var View = Spinal.namespace('com.spinal.ui.View', Spinal.com.spinal.core.Class.i
 	**/
 	initialize: function() {
 		return this;
-	}
+	},
+	
+	/**
+	*	Render View
+	*	@public
+	*	@chainable
+	*	@method render
+	*	@return {com.spinal.ui.View}
+	**/
+	render: function() { },
+	
+	/**
+	*	Update View
+	*	@public
+	*	@chainable
+	*	@method update
+	*	@return {com.spinal.ui.View}
+	**/
+	update: function() { },
+	
+	/**
+	*	Lookup
+	*	@public
+	*	@chainable
+	*	@method lookup
+	*	@return Any
+	**/
+	lookup: function() { },
+	
+	/**
+	*	Show View
+	*	@public
+	*	@chainable
+	*	@method show
+	*	@return {com.spinal.ui.View}
+	**/
+	show: function() { },
+	
+	/**
+	*	Hide View
+	*	@public
+	*	@chainable
+	*	@method hide
+	*	@return {com.spinal.ui.View}
+	**/
+	hide: function() { },
+	
+	/**
+	*	Enable View
+	*	@public
+	*	@chainable
+	*	@method enable
+	*	@return {com.spinal.ui.View}
+	**/
+	enable: function() { },
+	
+	/**
+	*	Disable View
+	*	@public
+	*	@chainable
+	*	@method disable
+	*	@return {com.spinal.ui.View}
+	**/
+	disable: function() { },
+	
+	/**
+	*	Clear View
+	*	@public
+	*	@chainable
+	*	@method clear
+	*	@return {com.spinal.ui.View}
+	**/
+	clear: function() { }
 	
 }, {
 	
@@ -29,6 +101,22 @@ var View = Spinal.namespace('com.spinal.ui.View', Spinal.com.spinal.core.Class.i
 	*	@property NAME
 	*	@type String
 	**/
-	NAME: 'View'
+	NAME: 'View',
+	
+	/**
+	*	@static
+	*	@property EVENTS
+	*	@type Object
+	**/
+	EVENTS: {
+		shown: 'com:spinal:ui:view:shown',
+		hidden: 'com:spinal:ui:view:hidden',
+		enabled: 'com:spinal:ui:view:enabled',
+		disabled: 'com:spinal:ui:view:disabled',
+		clicked: 'com:spinal:ui:view:clicked',
+		focused: 'com:spinal:ui:view:focused',
+		blurred: 'com:spinal:ui:view:blurred',
+		cleared: 'com:spinal:ui:view:cleared'
+	}
 	
 }));
