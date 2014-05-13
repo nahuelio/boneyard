d602d9b256138201713e4430ae966a689effa9e9
kuakman <3dimentionar@gmail.com>
2014-05-13 15:32:38 -0700

    Added karma-spec-reporter to package.json to display mocha spec reports in the temrinal.

--- karma.conf.js
+++ karma.conf.js
@@ -25,13 +25,15 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: { '*.js': ['covergae'] },
+    preprocessors: {
+        'target/**/*.js': ['coverage']
+    },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage'],
+    reporters: ['spec', 'coverage'],
 
     // optionally, configure the reporter
     coverageReporter: {
