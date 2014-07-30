cdd7b8a70d2eb24ef63ef3f1a4d3aaad00082442
kuakman <3dimentionar@gmail.com>
2014-07-29 22:11:05 -0700

    Refactored spinal composer tool. Added initial code for the composer tool (command line program).

--- run.js
+++ run.js
@@ -1,10 +1,13 @@
 /**
-*	Basic Web Service that shows Benchmarking and Code Coverage results
+*	Basic Web Service that servers content for
+*	Benchmarking, API docs, high level documentation (examples) and Spinal Composer.
 **/
 
-var connect = require('connect');
+var connect = require('connect'),
+	composer = require('./tools/composer');
 connect().use(connect.static(__dirname + '/target'))
 	.use(connect.static(__dirname + '/benchmark'))
 	.use(connect.static(__dirname + '/docs'))
 	.use(connect.static(__dirname + '/examples'))
+	//.use(connect.static(__dirname + '/composer')) Add later with an autowatch service on the source
 	.listen(8080);
