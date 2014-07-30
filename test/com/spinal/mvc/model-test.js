b0af018a80cefa14fff0982cb66f5cb858f5af88
kuakman <3dimentionar@gmail.com>
2014-07-30 11:06:51 -0700

    Added mvc unit test files for Controller, Model and Service.

---
+++ test/com/spinal/mvc/model-test.js
@@ -0,0 +1,30 @@
+/**
+*	com.spinal.mvc.Model Class Tests
+*	@author Patricio Ferreira <3dimentionar@gmail.com>
+**/
+define(['mvc/model'], function(Model) {
+
+	describe('com.spinal.mvc.Model', function() {
+
+		before(function() {
+			// TODO
+		});
+
+		after(function() {
+			// TODO
+		});
+
+		/**
+		*	Model#new() test
+		**/
+		describe('#new()', function() {
+
+			it('Should Instanciate Model Class', function() {
+
+			});
+
+		});
+
+	});
+
+});
---
+++ test/com/spinal/mvc/service-test.js
@@ -0,0 +1,30 @@
+/**
+*	com.spinal.mvc.Service Class Tests
+*	@author Patricio Ferreira <3dimentionar@gmail.com>
+**/
+define(['mvc/service'], function(Service) {
+
+	describe('com.spinal.mvc.Service', function() {
+
+		before(function() {
+			// TODO
+		});
+
+		after(function() {
+			// TODO
+		});
+
+		/**
+		*	Service#new() test
+		**/
+		describe('#new()', function() {
+
+			it('Should Instanciate Service Class', function() {
+
+			});
+
+		});
+
+	});
+
+});
