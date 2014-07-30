c0614621f513b659e92926d5f0982b5ec678e4a0
kuakman <3dimentionar@gmail.com>
2014-07-29 19:20:47 -0700

    Adding some more unit tests files to write later for the ioc package and factory utility class.

---
+++ test/com/spinal/ioc/bone-factory-test.js
@@ -0,0 +1,31 @@
+/**
+*	com.spinal.ioc.BoneFactory Class Tests
+*	@author Patricio Ferreira <3dimentionar@gmail.com>
+**/
+define(['ioc/context',
+		'ioc/bone-factory'], function(ContextBoneFactory) {
+
+	describe('com.spinal.ioc.BoneFactory', function() {
+
+		before(function() {
+			// TODO
+		});
+
+		after(function() {
+			// TODO
+		});
+
+		/**
+		*	BoneFactory#new() test
+		**/
+		describe('#new()', function() {
+
+			it('Should Instanciate BoneFactory Class', function() {
+
+			});
+
+		});
+
+	});
+
+});
---
+++ test/com/spinal/util/string-test.js
@@ -0,0 +1,30 @@
+/**
+*	com.spinal.util.StringUtil Class Tests
+*	@author Patricio Ferreira <3dimentionar@gmail.com>
+**/
+define(['util/string'], function(StringUtil) {
+
+	describe('com.spinal.util.StringUtil', function() {
+
+		before(function() {
+			// TODO
+		});
+
+		after(function() {
+			// TODO
+		});
+
+		/**
+		*	StringUtil#new() test
+		**/
+		describe('#new() -> (Static)', function() {
+
+			it('Should NOT instanciate StringUtil Class (Static)', function() {
+
+			});
+
+		});
+
+	});
+
+});
