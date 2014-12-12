e29745d773715ff90b390712edd3e4c9135a909f
kuakman <3dimentionar@gmail.com>
2014-12-12 12:03:18 -0800

    Forgot to add new ui classes into the spinal-ui bundle for exporting.

--- src/spinal-ui.js
+++ src/spinal-ui.js
@@ -3,4 +3,8 @@
 *	@author Patricio Ferreira <3dimentionar@gmail.com>
 **/
 define(['ui/view',
-		'ui/container'], function() { });
+		'ui/container',
+		'ui/basic/paragraph',
+		'ui/basic/link',
+		'ui/misc/panel',
+		'ui/form/controls/button'], function() { });
