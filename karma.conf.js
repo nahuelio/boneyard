4df539146d8b41e7dc8d131e6c545bf495041ee3
kuakman <3dimentionar@gmail.com>
2015-11-06 22:51:29 -0800

    Removed karma htmlfile reporter dependency.

--- karma.conf.js
+++ karma.conf.js
@@ -45,11 +45,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['nyan', 'html', 'coverage'],
-
-        htmlReporter: {
-            outputFile: 'coverage/unit-test-results.html'
-        },
+        reporters: ['nyan', 'coverage'],
 
         // optionally, configure the reporter
         coverageReporter: {
